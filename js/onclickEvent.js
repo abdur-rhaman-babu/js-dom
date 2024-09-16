7c85af7ac51da470458f01dc8bfe8da9e311a65d
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-09-16 22:11:28 +0600

    onclick all method done

--- js/onclickEvent.js
+++ js/onclickEvent.js
@@ -9,7 +9,28 @@ function makeBlueButton (){
     document.body.style.backgroundColor = 'blue'
 }
 
+
 const makePurple = document.getElementById('makePurple')
 makePurple.onclick = function makePurpleButton (){
     document.body.style.backgroundColor = 'purple'
 }
+
+
+const makeGreenButton = document.getElementById('make-green')
+makeGreenButton.addEventListener('click', makeGreen);
+
+function makeGreen(){
+    document.body.style.backgroundColor = 'green'
+}
+
+
+const makePinkButton = document.getElementById('make-pink');
+makePinkButton.addEventListener('click', function makePink(){
+    document.body.style.backgroundColor = 'pink'
+})
+
+
+document.getElementById('make-goldenrod').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'goldenrod'
+})
+
