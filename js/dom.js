74328b8d01fe2e800eca4e7e073d473f3b4bbc7b
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-09-16 12:25:03 +0600

    js dom initialize

---
+++ js/dom.js
@@ -0,0 +1,9 @@
+const listItem = document.getElementsByTagName('li')
+
+// for ( const list of listItem){
+//     console.log(list.innerText)
+//     console.log(list)
+// }
+
+document.getElementById('heading').innerText = 'Tour of js dom';
+
